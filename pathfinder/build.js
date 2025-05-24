482d7d22e64829d52dd382fb1d4f81c1f3e69d98
ItsDeltin <grittydeltin@gmail.com>
2025-05-24 18:02:40 -0500

    feat: overpy build script

---
+++ pathfinder/build.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+(async () => {
+    if (!fs.existsSync("overpy_standalone.js")) {
+        console.log("Downloading overpy_standalone.js");
+        const content = await fetch(
+            "https://raw.githubusercontent.com/ItsDeltin/overpy/refs/heads/master/out/overpy_standalone.js"
+        );
+        fs.writeFileSync("overpy_standalone.js", await content.text());
+    }
+    decompile();
+})();
+
+async function decompile() {
+    console.log("Decompiling scripts");
+    const overpy_standalone = require("./overpy_standalone.js");
+    await overpy_standalone.readyPromise;
+
+    function decompileFile(readFile, writeFile) {
+        const text = fs.readFileSync(readFile, { encoding: "utf8" });
+        let decompilation;
+        try {
+            decompilation = overpy_standalone.decompileAllRules(
+                text,
+                "en-US",
+                { ignoreVariableIndex: true, ignoreSubroutineIndex: true }
+            );
+        }
+        catch (ex) {
+            console.log(`Error while decompiling ${readFile}: ${ex}`);
+            return;
+        }
+        fs.writeFileSync(writeFile, decompilation)
+        console.log(`${readFile} -> ${writeFile}`);
+    }
+
+    decompileFile("core.ow", "core.opy");
+    decompileFile("compression/compression.ow", "compression/compression.opy");
+}
